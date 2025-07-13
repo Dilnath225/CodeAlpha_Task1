0381bf041229ddbb820b11727cf8b09d594c6ff8
Dilnath225 <sunerarathnayaka@gmail.com>
2025-07-13 11:39:34 +0530

    check

---
+++ Task1.py
@@ -0,0 +1,10 @@
+"""Hangman Game in Python
+This is a simple implementation of the classic Hangman game.
+Players guess letters to figure out a hidden word, with a limited number of incorrect guesses allowed.
+
+Author: Rathnayaka Pathiranage Sunera Dilnath
+Student ID: CA/JU1/26435
+Date: 2025- 07-11
+Location: Sri Lanka
+
+"""
