e11fcc9e526582f28405d6ef04940fe57ded9999
Dilnath225 <sunerarathnayaka@gmail.com>
2025-07-13 11:43:19 +0530

    check

--- Task1.py
+++ Task1.py
@@ -8,3 +8,24 @@ Date: 2025- 07-11
 Location: Sri Lanka
 
 """
+
+import random
+
+def hangman():
+
+    word_lst = [apple, banana, cherry, date, chair]
+    word = random.choice(word_lst)
+
+    guessed_letters = []
+    incorrect_guesses = 0
+    max_attempts = 6
+
+    print("Welcome to Hangman!")
+    print("_ " * len(word))  #displaying blanks for each letter in the word
+
+
+    #creating game loops
+    while incorrect_guesses < max_attempts:
+        guess = input("\nGuess a letter: ").lower()
+
+        
